93ba856cd21d27e5497879de4222ab94fa25f0a1
Morgan Harmon <morganharmon@gmail.com>
2022-04-25 16:38:33 -0700

    add tallies and grab DOMs

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 // import functions and grab DOM elements
+const shell1 = document.getElementById('shell1');
+const shell2 = document.getElementById('shell2');
+const shell3 = document.getElementById('shell3');
+
+const pearl1 = document.getElementById('pearl1');
+const pearl2 = document.getElementById('pearl2');
+const pearl3 = document.getElementById('pearl3');
+
+const pearls = document.getElementById('pearls');
+const empties = document.getElementById('empties');
+const total = document.getElementById('total');
 
 // let state
 
