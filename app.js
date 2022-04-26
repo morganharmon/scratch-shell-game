a20e743d54b2bbfa9bd599eef05ba2a5ed1920d6
Morgan Harmon <morganharmon@gmail.com>
2022-04-25 17:43:00 -0700

    Add event listeners to buttons

--- app.js
+++ app.js
@@ -7,13 +7,50 @@ const pearl1 = document.getElementById('pearl1');
 const pearl2 = document.getElementById('pearl2');
 const pearl3 = document.getElementById('pearl3');
 
-const pearls = document.getElementById('pearls');
-const empties = document.getElementById('empties');
-const total = document.getElementById('total');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
 
 // let state
+let pearls = 0;
+let empties = 0;
+let total = pearls + empties;
 
 // set event listeners 
+button1.addEventListener('click', () => {
+    const winner = Math.ceil(Math.random() * 3);
+    if (winner === 1) {
+        alert('You found a pearl!');
+        pearls++;
+
+    } else {
+        alert('Better luck next time.');
+        empties++;
+    }
+});
+
+button2.addEventListener('click', () => {
+    const winner = Math.ceil(Math.random() * 3);
+    if (winner === 2) {
+        alert('You found a pearl!');
+        pearls++;
+
+    } else {
+        alert('Better luck next time.');
+        empties++;
+    }
+});
+button3.addEventListener('click', () => {
+    const winner = Math.ceil(Math.random() * 3);
+    if (winner === 3) {
+        alert('You found a pearl!');
+        pearls++;
+
+    } else {
+        alert('Better luck next time.');
+        empties++;
+    }
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
